8c1b471e752bc632907162cb057e84e9430794c4
Henry Zhang <henz51111@gmail.com>
2024-02-09 23:32:44 -0800

    add basic UI functionality until form submission

--- src/exchange-rate-service.js
+++ src/exchange-rate-service.js
@@ -1,5 +1,18 @@
 export default class ExchangeRateService {
-  
+  static async getRate(country) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      const jsonResponse = await response.json();
+      if(!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}
+        ${jsonResponse.error-type}`;
+        throw new Error(errorMessage);
+      }
+      return jsonResponse;
+    } catch(error) {
+      return error;
+    }
+  }
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 import './css/styles.css';
 import ExchangeRateService from './exchange-rate-service.js';
 
+// business logic
+
+
+
+
+
+
+function isValid(input) {
+  let status = true;
+  let counter = 0;
+  input.split("").forEach((digit) => {
+    if(digit === ".") {
+        counter++;
+        if(counter >= 2) {
+          status = false;
+        }
+    } else if(/\D/.test(digit)) {
+      status = false;
+    }
+  });
+  return status;
+}
+// UI logic
+
+
+
+function handleInputChange() {
+  if(!document.getElementById("amountUSD").value) {
+    document.getElementById("currency").disabled = true;
+    document.getElementById("submit").disabled = true;
+  } else if(isValid(document.getElementById("amountUSD").value)) {
+    document.getElementById("currency").disabled = false;
+    document.getElementById("submit").disabled = false;
+    document.getElementById("error1").setAttribute("class", "hidden");
+  } else {
+    document.getElementById("currency").disabled = true;
+    document.getElementById("submit").disabled = true;
+    document.getElementById("error1").removeAttribute("class");
+  }
+}
+
+function handleExchange(e) {
+  e.preventDefault();
+  alert("asdf");
+}
+
+
+document.getElementById("amountUSD").addEventListener("input", handleInputChange);
+document.getElementById("exchange").addEventListener("submit", handleExchange);
+
+
